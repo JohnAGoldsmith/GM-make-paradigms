1ea6a3846d366ee7cbd2754d40431b4dcaef1410
JohnAGoldsmith <goldsmith@uchicago.edu>
2016-12-23 15:09:14 -0600

    Works much better now. John.

--- readparadigmdata.py
+++ readparadigmdata.py
@@ -7,87 +7,105 @@
 # I should use a CSV reading package, which exists in python.
 
 import numpy as np
+import csv 
+
 np.set_printoptions(suppress=True)
+np.set_printoptions(precision=2)
+
  
 class CParadigm:
-        paradigm_space_dict = {}                # key is  a tuple of a set of feature values, value is a morph
-        list_of_feature_value_labels = []       # a list of set of feature values, one for each row of TPM
-        list_of_morpheme_indices = []           # a list of the number of the morpheme for each row in the TPM
+        FV_tuple_to_morph = {}             # key is  a tuple of a set of feature values, value is a morph
+        row_number_to_set_of_FVs =[]
+        row_number_to_morph_number =[]
         feature_values = {}                     # key is name of feature-value; value is its count
-        morpheme_dict = {}                      # key is morpheme
-        morpheme_list= []    
+        morpheme_to_index = {}                      # key is morpheme and value is an integer
+        morpheme_number_to_morph= []    
         TPM_length = 0                          # number of entries in TPM (in paradigm)
         TPM = np.zeros
+        
+        def readparadigm(self,lines):
 
-        def readparadigm(self,f, items):
-                linecount = 0
-                for line in f:
-                  line = line.strip()
-                  if len(line) == 0:
-                     break
-                  items = line.split()
-                  morph_number = int(items.pop())
-                  morph = self.morpheme_list[morph_number]
-                  num_items = len(items)
-                  if num_items > 1:
-                        linecount += 1
-                        feature_set = set(items)
-                        self.list_of_feature_value_labels.append(feature_set)
-                        print ("line count:", linecount, feature_set)
-                        self.paradigm_space_dict[tuple(feature_set)] = morph
-                        #print ("set", " ",feature_set)
-                        for itemno in range(num_items):
-                                thisfv = items[itemno]
-                                if thisfv not in self.feature_values:
-                                        self.feature_values[thisfv] = 1
-                                else:
-                                        self.feature_values[thisfv] += 1
-                        self.list_of_morpheme_indices.append(morph_number)
-                else:
-                       print ("error: ", line, len(line), items, num_items)
-                self.TPM_length = linecount
-                self.TPM = np.zeros((linecount,number_of_morphemes))
-                for row_no in range(linecount):
-                        print (row_no, self.list_of_morpheme_indices[row_no])
-                        self.TPM[row_no, self.list_of_morpheme_indices[row_no]] = 1
-                print (self.TPM, "\n ---------\n")
+                for lineno in range(len(lines)):
+                   line = lines[lineno]
+                   items = line.split()
+                   if items[0] == "#":
+                      if items[1] == "pattern":
+                         pattern_label = items[2]
+                      else:
+                         morphemes = items[1:]
+                         number_of_morphemes = len(morphemes)
+                         for morphno in range(len(morphemes)):
+                            thismorph = morphemes[morphno] 
+                            self.morpheme_number_to_morph.append(thismorph)
+                            self.morpheme_to_index[thismorph] = morphno
+                   else:
+                       morph = items.pop(-1)
+                       morphindex=thisparadigm.morpheme_to_index[morph]
+                       feature_set=set(items)
+                       self.row_number_to_set_of_FVs.append(feature_set)      
+                       self.FV_tuple_to_morph[tuple(feature_set)] = morph
+                       for itemno in range(len(items)):
+                          thisfv = items[itemno]
+                          if thisfv not in thisparadigm.feature_values:
+                             self.feature_values[thisfv] = 1
+                          else:
+                             self.feature_values[thisfv] += 1
+                       self.row_number_to_morph_number.append(morphindex)
+                 
+                numberofrows = len(self.row_number_to_set_of_FVs)
+                self.TPM_length = numberofrows
+                self.TPM = np.zeros((numberofrows,number_of_morphemes))
+                for row_no in range(numberofrows):
+                   self.TPM[row_no, thisparadigm.row_number_to_morph_number[row_no]] = 1
+
+               
+
+       
 
 
         def printparadigm(self):
-                for entry in self.list_of_feature_value_labels:
-                        print ("A:", entry, self.paradigm_space_dict[tuple(entry)])
+                print (thisparadigm.TPM )
+                number_of_rows= len(self.row_number_to_morph_number)
+
+                print ("List of feature value labels and paradigm space dict:")
+                for entry in self.row_number_to_set_of_FVs:
+                        tuple_of_entry = tuple(entry)
+                        this_morph=self.FV_tuple_to_morph[tuple_of_entry]
+                        print (entry, this_morph, self.morpheme_to_index[this_morph])
+                print ("List of morpheme indices")
+                for no in  range(number_of_rows) :
+                        morph_number = self.row_number_to_morph_number[no]
+                        morph = self.morpheme_number_to_morph[morph_number]
+                        morph_number_recompute = self.morpheme_to_index[morph]
+                        print ("row: ", no,"   ",morph_number, morph, morph_number_recompute  )
                 Y,s,Vh = np.linalg.svd(self.TPM)
-                print ("Y\n\n",Y)
-                print ("s\n\n",s)
-                print("Vh\n\n",Vh)
+                #print ("Y\n\n",Y)
+                #print ("\ns\n\n",s)
+                #print("Vh",Vh)
                 pi = np.linalg.pinv(self.TPM)
                 print ("pseudoinverse\n\n", pi)
+                print ("\npseudoinverse times matrix\n\n", np.matmul(pi,self.TPM))
+                print ("\nmatrix times pseudoinverse \n\n", np.matmul(self.TPM,pi))
 
 
+print (" --------- Part 1---------------")
 
-np.set_printoptions(precision=2)
+filename = "russiannouns1.txt"
 
 thisparadigm=CParadigm()
-with open('russiannouns.txt', 'r') as f:
-  listofmorphemes= f.readline()
-  morphemes = listofmorphemes.split()
-  number_of_morphemes= len(morphemes)
-  for morphno in range(len(morphemes)):
-    thismorph = morphemes[morphno]
-    thisparadigm.morpheme_list.append(thismorph)
-    thisparadigm.morpheme_dict[thismorph] = 1
-  while True:
-          items = f.readline().strip().split()
-          if len(items) == 0:
-            break
-          if (items[0][0] == "\\"):
-            if (items[0][1:8] =="pattern"):
-              name_of_pattern = items[1]
-              thisparadigm = CParadigm()
-              thisparadigm.readparadigm(f,items)
-              thisparadigm.printparadigm()
+ 
+lines = [line.strip() for line in open(filename)]
+
+thisparadigm.readparadigm(lines)
+
+ 
+thisparadigm.printparadigm()
 
 
+
+ 
+ 
+ 
 # example:
 #nul a e u om y ov ax am ami j i  
 #\pattern 1
@@ -104,26 +122,5 @@ with open('russiannouns.txt', 'r') as f:
 #dat pl 8
 #inst pl 9
 
-
-
-paradigm2=CParadigm()
-with open('russiannouns2.txt', 'r') as f:
-  listofmorphemes= f.readline()
-  morphemes = listofmorphemes.split()
-  number_of_morphemes= len(morphemes)
-  for morphno in range(len(morphemes)):
-    thismorph = morphemes[morphno]
-    paradigm2.morpheme_list.append(thismorph)
-    paradigm2.morpheme_dict[thismorph] = 1
-  while True:
-          items = f.readline().strip().split()
-          if len(items) == 0:
-            break
-          if (items[0][0] == "\\"):
-            if (items[0][1:8] =="pattern"):
-              name_of_pattern = items[1]
-              paradigm2 = CParadigm()
-              paradigm2.readparadigm(f,items)
-              paradigm2.printparadigm()
-
+ 
        
